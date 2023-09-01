29ac68d6775a7e7d3f52f3bfa03fab6a6308f465
GaimerGai <devinmrichards95@gmail.com>
2023-08-31 20:22:46 -0700

    Refactor to add firstName and lastName

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -36,7 +36,17 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
           len: [60, 60]
         }
-      }
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        isAlpha:true,
+      },
+      lastName: {
+        type:DataTypes.STRING,
+        allowNull:false,
+        isAlpha:true,
+      },
     },
     {
       sequelize,
