13a3a7ea21e8111cfb6e706a928b67460b0037fd
GaimerGai <devinmrichards95@gmail.com>
2023-11-25 22:24:46 -0700

    styling

--- frontend/src/components/Groups/GroupDetail.js
+++ frontend/src/components/Groups/GroupDetail.js
@@ -62,75 +62,85 @@ function GroupDetail() {
   return (
     isLoaded && (
       <div className="web-page">
-        <div className="backlink">
-          <Link to="/groups">Groups</Link>
+        <div className="group-info-container">
+          <div className="backlink">
+            <Link to="/groups">Groups</Link>
+          </div>
+          <div className="group-details">
+            <h2>{groupData.name}</h2>
+            <img src={groupData.previewImage} alt="Group Preview" />
+            <h3>
+              {groupData.city}, {groupData.state}
+            </h3>
+            <p>Number of Events: {groupData.numEvents} • {isPrivate}</p>
+            <p>Organized by: {groupData.Organizer.firstName} {groupData.Organizer.lastName}</p>
+            {session.user?.id !== groupData.Organizer.id && session.user && (
+              <button className="join-group-button" onClick={handleJoinGroup}>
+                Join this group
+              </button>
+            )}
+            <button onClick={handleCreateEvent}>Create An Event</button>
+            <button>
+              <Link to={`/groups/${groupData.id}/edit`}>Update</Link>
+            </button>
+            <button>
+              <OpenModalMenuItem
+                itemText="Delete"
+                onItemClick={closeMenu}
+                modalComponent={<DeleteConfirmationModal entityType='group' />}
+              />
+            </button>
+          </div>
         </div>
-        <div className="topCard">
-          <h2>{groupData.name}</h2>
-          <img src={groupData.previewImage} alt="Group Preview" />
-          <h3>
-            {groupData.city}, {groupData.state}
-          </h3>
-          <p>Number of Events: {groupData.numEvents} * {isPrivate}</p>
-          <p>
-            Organized by: {groupData.Organizer.firstName} {groupData.Organizer.lastName}
-          </p>
-          {
-            (session.user?.id !== groupData.Organizer.id && session.user) &&(
-              <button className="join-group-button" onClick={handleJoinGroup}>Join this group</button>
-            )
-          }
-          <button onClick={handleCreateEvent}>Create An Event</button>
-          <button>
-          <Link to={`/groups/${groupData.id}/edit`}>
-          Update
-          </Link>
-          </button>
-          <button>
-          <OpenModalMenuItem
-          itemText="Delete"
-          onItemClick={closeMenu}
-          modalComponent={<DeleteConfirmationModal entityType='group'/>}
-          />
-          </button>
-        </div>
-        <div className="middleCard">
-          <h2>Organizer</h2>
-          <p>{groupData.Organizer.firstName} {groupData.Organizer.lastName}</p>
-          <h2>What We're about</h2>
-          <p>{loremIpsum}</p>
-        </div>
-        <div className="eventsCard">
-          {upcomingEvents.length > 0 && (
-            <div className="upcoming-Events">
-              <h2>Upcoming Events</h2>
-              {upcomingEvents.map((event) => (
-                <div key={event.id} className="event-card">
-                  <h3>Title: {event.name}</h3>
-                  <p>Date: {new Date(event.startDate).toLocaleDateString()}</p>
-                  <p>Time: {new Date(event.startDate).toLocaleTimeString()}</p>
-                  <img src={event.previewImage} alt={event.name} />
-                  <p>Location: {event.Venue ? event.Venue.city : "Online"}</p>
-                  <p>Description: {event.description}</p>
-                </div>
-              ))}
-            </div>
-          )}
-          {pastEvents.length > 0 && (
-            <div className="past-Events">
-              <h2>Past Events</h2>
-              {pastEvents.map((event) => (
-                <div key={event.id} className="event-card">
-                  <h3>Title: {event.name}</h3>
-                  <p>Date: {new Date(event.startDate).toLocaleDateString()}</p>
-                  <p>Time: {new Date(event.startDate).toLocaleTimeString()}</p>
-                  <img src={event.previewImage} alt={event.name} />
-                  <p>Location: {event.Venue ? event.Venue.city : "Online"}</p>
-                  <p>Description: {event.description}</p>
-                </div>
-              ))}
-            </div>
-          )}
+
+        <div className="group-details-container">
+          <div className="organizer-info">
+            <h2>Organizer</h2>
+            <p>{groupData.Organizer.firstName} {groupData.Organizer.lastName}</p>
+          </div>
+
+          <div className="about-info">
+            <h2>What We're About</h2>
+            <p>{groupData.about}</p>
+          </div>
+
+          <div className="events-container">
+            {upcomingEvents.length > 0 && (
+              <div className="upcoming-events">
+                <h2>Upcoming Events</h2>
+                {upcomingEvents.map((event) => (
+                  <div key={event.id} className="event-card">
+                    <img src={event.previewImage} alt={event.name} />
+                    <div className="event-details">
+                      <p>Date: {new Date(event.startDate).toLocaleDateString()}</p>
+                      <p>Time: {new Date(event.startDate).toLocaleTimeString()}</p>
+                      <h3>{event.name}</h3>
+                      <p>Location: {event.Venue ? event.Venue.city : "Online"}</p>
+                      <p>{event.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+
+            {pastEvents.length > 0 && (
+              <div className="past-events">
+                <h2>Past Events</h2>
+                {pastEvents.map((event) => (
+                  <div key={event.id} className="event-card">
+                    <img src={event.previewImage} alt={event.name} />
+                    <div className="event-details">
+                      <p>Date: {new Date(event.startDate).toLocaleDateString()}</p>
+                      <p>Time: {new Date(event.startDate).toLocaleTimeString()}</p>
+                      <h3>{event.name}</h3>
+                      <p>Location: {event.Venue ? event.Venue.city : "Online"}</p>
+                      <p>{event.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     )
